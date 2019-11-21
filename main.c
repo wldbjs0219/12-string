cdcc530bd0bd23f1b3354773f29f75a7b23b8f3e
wldbjs0219 <55428633+wldbjs0219@users.noreply.github.com>
2019-11-21 15:28:36 +0900

    12-6

--- main.c
+++ main.c
@@ -6,16 +6,19 @@
 
 int main(void) {
 	int i;
-	char input[100];
+	char c;
 	FILE* fp;
 	
 	fp=fopen("sample.txt","r");
-	for (i=0;i<3;i++)
+	if(fp==NULL)
 	{
-		//printf("input a string: ");
-		//scanf("%s",input);
-		fscanf(fp, "%s", input);
-		printf("%s\n", input);
+		printf("wrong path!");
+		return -1;
+	}
+	
+	while( (c=fgetc(fp)) != EOF)
+	{
+		putchar(c);
 	}
 	
 	fclose(fp);
